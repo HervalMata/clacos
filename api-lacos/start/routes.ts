6f0ecd469ec86c0113b2ee9309ef939bd0d8566e
Herval Mata <hervalmataamparo@gmail.com>
2022-08-16 20:11:45 -0300

    feat:art: clacos - created client controller and validator

--- api-lacos/start/routes.ts
+++ api-lacos/start/routes.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/login', 'AuthController.login')
 Route.post('/logout', 'AuthController.logout')
+Route.post('/client/register', 'ClientsController.store')
 Route.group(() => {
   Route.get('auth/me', 'AuthController.me');
 }).middleware('auth');
