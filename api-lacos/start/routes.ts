648b16160012a4e6e97122a52530a9e425e7d3c2
Herval Mata <hervalmataamparo@gmail.com>
2022-08-16 12:01:31 -0300

    feat:art: clacos - created login route

--- api-lacos/start/routes.ts
+++ api-lacos/start/routes.ts
@@ -1,22 +1,4 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 
 import Route from '@ioc:Adonis/Core/Route'
 import User from 'App/Models/user'
@@ -28,3 +10,23 @@ Route.get('/', async () => {
     type: "admin",
   });
 });
+
+Route.post('/login', async ({ request, response, auth }: HttpContextContract) => {
+  const email = request.input('email');
+  const password = request.input('password');
+
+  const user = await User.findBy('email', email);
+
+  if (user == null) {
+    return response.notFound("Usuário não encontrado.");
+  }
+
+  const token = await auth.use('api').attempt(email, password);
+
+
+  return response.ok(token);
+});
+
+Route.get('/auth', async ({ response }: HttpContextContract) => {
+  return response.ok("Somente usuários autenticados podem acessar.");
+}).middleware('api');
