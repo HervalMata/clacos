7707eb76a43c134b7201bce5b60bc15a6dbb02ae
Herval Mata <hervalmataamparo@gmail.com>
2022-08-16 19:20:28 -0300

    feat:art: clacos - created method me for auth controller

--- api-lacos/app/Controllers/Http/AuthController.ts
+++ api-lacos/app/Controllers/Http/AuthController.ts
@@ -1,5 +1,8 @@
+import Shop from 'App/Models/Shop';
 import User from 'App/Models/user';
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import Client from 'App/Models/Client';
+import Admin from 'App/Models/Admin';
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 export default class AuthController {
@@ -48,5 +51,49 @@ export default class AuthController {
     }
 
     return response.ok({ revoked: true });
+  }
+
+  public async me({ auth, response }: HttpContextContract) {
+    const userAuth = await auth.use('api').authenticate()
+
+    let data;
+
+    switch (userAuth.type) {
+      case 'clients':
+        const client = await Client.findByOrFail('userId', userAuth.id)
+        data = {
+          id_client: client.id,
+          name: client.name,
+          phone: client.phone,
+          email: userAuth.email,
+        }
+        break;
+
+      case 'shops':
+        const shop = await Shop.findByOrFail('userId', userAuth.id)
+        data = {
+          id_shop: shop.id,
+          name: shop.name,
+          logo: shop.logo,
+          online: shop.online,
+          blocked: shop.blocked,
+          email: userAuth.email,
+        }
+        break;
+
+      case 'admins':
+        const admin = await Admin.findByOrFail('userId', userAuth.id)
+        data = {
+          id_admin: admin.id,
+          name: admin.name,
+          email: userAuth.email,
+        }
+        break;
+
+      default:
+        return response.unauthorized('Unauthorized user - type not found');
+    }
+
+    return response.ok(data);
   }
 }
--- api-lacos/start/routes.ts
+++ api-lacos/start/routes.ts
@@ -2,6 +2,9 @@ import Route from '@ioc:Adonis/Core/Route'
 
 Route.post('/login', 'AuthController.login')
 Route.post('/logout', 'AuthController.logout')
+Route.group(() => {
+  Route.get('auth/me', 'AuthController.me');
+}).middleware('auth');
 
 Route.get('/', async () => {
   return {
