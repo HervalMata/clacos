7e4c537303557c114a79d47f8fe48a97765d8cb4
Herval Mata <hervalmataamparo@gmail.com>
2022-08-16 12:32:29 -0300

    feat:art: clacos - created migrations for states and cities

---
+++ api-lacos/database/migrations/1660663587797_cities.ts
@@ -0,0 +1,18 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'cities'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.integer('state_id').unsigned().notNullable().references('id').inTable('states').onDelete('CASCADE')
+      table.string('name', 255).notNullable()
+      table.boolean('active').notNullable().defaultTo(true)
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
